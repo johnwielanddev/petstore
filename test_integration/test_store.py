eea9e79c00a622ddc3e1c145282e0736d640f257
John Wieland <johnny.c.sparkles@gmail.com>
2019-09-04 21:35:20 +1000

    Added a more robust assertion to the /store/inventory test

--- test_integration/test_store.py
+++ test_integration/test_store.py
@@ -1,12 +1,11 @@
 from .testing_client import TestCase
+from api.store import store_objs
 
 
 class StoreAPITestCase(TestCase):
-
-
   def test_get_store_inventory_success(self):
     response = self.app.get('/store/inventory/')
 
     self.assertEquals(response.status_code, 200)
-
+    self.assertEquals(response.json, store_objs)
 
