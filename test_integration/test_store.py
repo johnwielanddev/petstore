b0709665944a4e2d47f68777a76c1f7a3d97db5e
John Wieland <johnny.c.sparkles@gmail.com>
2019-09-04 22:13:06 +1000

    Added the test and code for failed order POST

--- test_integration/test_store.py
+++ test_integration/test_store.py
@@ -25,3 +25,20 @@ class StoreAPITestCase(TestCase):
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.json, new_order)
 
+  def test_failed_order_invalid_id(self):
+    new_order = {
+      "id": "0",
+      "petId": 0,
+      "quantity": 0,
+      "shipDate": "2019-09-04T11:37:27.985Z",
+      "status": "placed",
+      "complete": False,
+    }
+
+    response = self.app.post('/store/order', json=new_order,
+                             follow_redirects=True)
+
+    self.assertEqual(response.status_code, 400)
+
+
+
