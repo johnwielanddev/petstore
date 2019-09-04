2c755a84b8d3536d4006d9698b43b6442c91ead8
John Wieland <johnny.c.sparkles@gmail.com>
2019-09-04 22:30:12 +1000

    Added a test for the get_order_by_id failure case

--- api/store.py
+++ api/store.py
@@ -15,4 +15,21 @@ def order(order):
   orders.append(order)
   return order
 
+def get_orders():
+  return orders
+
+def find_order_by_id(order_id):
+  for order in get_orders():
+    if (order['id'] == order_id):
+      return order
+
+  return None
+
+
+def get_order_by_id(order_id):
+  if find_order_by_id(order_id) != None:
+      return order, 200
+
+  return None, 404 
+
 
--- test_unit.py
+++ test_unit.py
@@ -1,10 +1,50 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 
-from api.store import get, store_objs
+from api.store import get, store_objs, find_order_by_id, orders 
+
+test_orders = [
+  {
+    "id": 0,
+    "petId": 0,
+    "quantity": 0,
+    "shipDate": "2019-09-04T11:37:27.985Z",
+    "status": "placed",
+    "complete": False,
+  },
+  {
+    "id": 3,
+    "petId": 0,
+    "quantity": 0,
+    "shipDate": "2019-09-04T11:37:27.985Z",
+    "status": "placed",
+    "complete": False,
+  },
+  {
+    "id": 99,
+    "petId": 0,
+    "quantity": 0,
+    "shipDate": "2019-09-04T11:37:27.985Z",
+    "status": "placed",
+    "complete": False,
+  },
+]
+
 
 class StoreInventoryTest(TestCase):
   def test_get_store_inventory(self):
     self.assertEqual(get(), store_objs)
+
+  @mock.patch('api.store.get_orders')
+  def test_find_order_by_id_success(self, mock_orders):
+    mock_orders.return_value = test_orders
+
+    self.assertEqual(find_order_by_id(3), test_orders[1])
+
+  @mock.patch('api.store.get_orders')
+  def test_find_order_by_id_fail(self, mock_orders):
+    mock_orders.return_value = test_orders
+
+    self.assertEqual(find_order_by_id(55), None)
 
 
 
